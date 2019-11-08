0fd7b42df4b03d2718fe0960b279e76b917e8232
Deva <trdevendran@gmail.com>
2019-11-08 16:55:33 +0530

    All readings of the regions displays when tap marker

--- app/src/main/java/com/trdevendran/assignmentpsi/util/CommonSnippets.kt
+++ app/src/main/java/com/trdevendran/assignmentpsi/util/CommonSnippets.kt
@@ -5,6 +5,7 @@ import android.content.DialogInterface
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import android.text.TextUtils
+import android.view.View
 import android.view.Window
 import androidx.appcompat.app.AlertDialog
 
@@ -36,13 +37,39 @@ class CommonSnippets {
          * @param message Shows the description/detailed message on the dialog window
          * @param buttonText It represents the text of positive button
          * @param onClickListener It handles the event of positive button of an AlertDialog
+         * @return AlertDialog to show the informative content as an alert
          */
         fun createAlertDialog(
             context: Context, title: String, message: String,
             buttonText: String, onClickListener: DialogInterface.OnClickListener
         ): AlertDialog {
+            return createAlertDialogWithView(
+                context,
+                title,
+                message,
+                buttonText,
+                onClickListener,
+                null
+            )
+
+        }
+
+        /**
+         * @param context of the current Activity
+         * @param title shows the short text/ purpose of the dialog window appears
+         * @param message Shows the description/detailed message on the dialog window
+         * @param buttonText It represents the text of positive button
+         * @param onClickListener It handles the event of positive button of an AlertDialog
+         * @param view uses to show the dynamic
+         * @return AlertDialog to show the informative content as an alert
+         */
+        fun createAlertDialogWithView(
+            context: Context, title: String, message: String,
+            buttonText: String, onClickListener: DialogInterface.OnClickListener, view: View?
+        ): AlertDialog {
             val alertDialog = AlertDialog.Builder(context).setTitle(title)
                 .setMessage(message)
+                .setView(view)
                 .setCancelable(true)
                 .setPositiveButton(buttonText, onClickListener)
                 .create()
@@ -50,6 +77,7 @@ class CommonSnippets {
                 alertDialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
             }
             return alertDialog
+
         }
     }
 }
